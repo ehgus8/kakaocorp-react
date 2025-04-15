b39f2e2fcb21a7c2bc2a2adf5c2609a7b21c08ff
ubing <ubing0101@kakao.com>
2025-04-15 12:02:35 +0900

    footer - 카피레이터, 패밀리사이트 추가

--- src/layouts/RootLayout.jsx
+++ src/layouts/RootLayout.jsx
@@ -1,16 +1,21 @@
 import React, { useEffect, useState } from 'react';
-import { Outlet, Link, useLocation } from 'react-router-dom';
+import { Link, Outlet } from 'react-router-dom';
 
 import styles from './RootLayout.module.scss';
 import MainNav from '../assets/components/MainNav';
 
-import SearchModal from '../components/Modal/SearchModal';
-import SearchPage from '../components/Search/SearchPage';
 import footerData from '../assets/components/footerData';
+import footerGroupData from '../assets/components/footerGroupData';
 
 const RootLayout = () => {
   const [isScrolled, setIsScrolled] = useState(false);
   const [openMenu, setOpenMenu] = useState(null);
+
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleToggle = () => {
+    setIsOpen((prev) => !prev);
+  };
 
   useEffect(() => {
     const handleScroll = () => {
@@ -24,23 +29,11 @@ const RootLayout = () => {
     };
   }, []);
 
-  // 모달 공개 여부 상태 변수
-  const [searchIsShown, setSearchIsShown] = useState(false);
-
-  // 모달을 열고 닫아 주는 핸들러
-  const showSearchHandler = () => setSearchIsShown(true);
-  const hideSearchHandler = () => setSearchIsShown(false);
-
-  const location = useLocation();
-  const queryParams = new URLSearchParams(location.search);
-  const searchQuery = queryParams.get('searchKeyword');
-
   const excludedIds = ['mainMenu-99']; // 투자정보를 제외하기 위해서
   const isOpenGnb = openMenu && !excludedIds.includes(openMenu);
 
   return (
     <div className={styles.layout}>
-      {searchIsShown && <SearchModal onClose={hideSearchHandler} />}
       <header
         className={`${styles['doc-header']} ${isScrolled ? styles.scroll : ''} ${isOpenGnb ? styles.open_gnb : ''}`}
       >
@@ -51,7 +44,7 @@ const RootLayout = () => {
 
       <main className={styles.main}>
         {/* 바뀌는 부분(동적 컴포넌트)이 들어갈 자리 */}
-        {searchQuery ? <SearchPage /> : <Outlet />}
+        <Outlet />
       </main>
 
       <footer className={styles.footer}>
@@ -130,7 +123,135 @@ const RootLayout = () => {
             })}
           </section>
 
-          <section></section>
+          <section>
+            <div className={styles.group_info}></div>
+            <div
+              className={`${styles.wrap_relation} ${isOpen ? styles.info_open : ''}`}
+            >
+              <strong className={styles.tit_relation} onClick={handleToggle}>
+                관련사이트
+                <svg
+                  xmlns='http://www.w3.org/2000/svg'
+                  viewBox='0 0 14 14'
+                  width='14'
+                  height='14'
+                  className={styles.ico_plus}
+                >
+                  <line
+                    x1='7'
+                    y1='0'
+                    x2='7'
+                    y2='14'
+                    stroke='currentColor'
+                    strokeWidth='1'
+                  />
+                  <line
+                    x1='0'
+                    y1='7'
+                    x2='14'
+                    y2='7'
+                    stroke='currentColor'
+                    strokeWidth='1'
+                  />
+                </svg>
+              </strong>
+              <ul className={styles.list_subinfo}>
+                <li>
+                  <Link to={'https://privacy.kakao.com/main?lang=ko'}>
+                    <span>카카오 프라이버시</span>
+                    <svg
+                      xmlns='http://www.w3.org/2000/svg'
+                      viewBox='0 0 9 9'
+                      className={styles.ico_outlink}
+                    >
+                      <g fill='none'>
+                        <path
+                          d='M1.795 1.074L7.942 1.074 7.942 7.221M7.942 1.074L1.378 7.638'
+                          transform='translate(-935 -867) translate(836 848) translate(14 14) translate(85 5)'
+                        ></path>
+                      </g>
+                    </svg>
+                  </Link>
+                </li>
+                <li>
+                  <Link to={'https://developers.kakao.com/'}>
+                    <span>카카오 디벨로퍼스</span>
+                    <svg
+                      xmlns='http://www.w3.org/2000/svg'
+                      viewBox='0 0 9 9'
+                      className={styles.ico_outlink}
+                    >
+                      <g fill='none'>
+                        <path
+                          d='M1.795 1.074L7.942 1.074 7.942 7.221M7.942 1.074L1.378 7.638'
+                          transform='translate(-935 -867) translate(836 848) translate(14 14) translate(85 5)'
+                        ></path>
+                      </g>
+                    </svg>
+                  </Link>
+                </li>
+                <li>
+                  <Link to={'https://www.daum.net/'}>
+                    <span>다음 포털 사이트</span>
+                    <svg
+                      xmlns='http://www.w3.org/2000/svg'
+                      viewBox='0 0 9 9'
+                      className={styles.ico_outlink}
+                    >
+                      <g fill='none'>
+                        <path
+                          d='M1.795 1.074L7.942 1.074 7.942 7.221M7.942 1.074L1.378 7.638'
+                          transform='translate(-935 -867) translate(836 848) translate(14 14) translate(85 5)'
+                        ></path>
+                      </g>
+                    </svg>
+                  </Link>
+                </li>
+                <li>
+                  <Link to={'https://winwin.kakao.com/index'}>
+                    <span>동반 성장 사이트</span>
+                    <svg
+                      xmlns='http://www.w3.org/2000/svg'
+                      viewBox='0 0 9 9'
+                      className={styles.ico_outlink}
+                    >
+                      <g fill='none'>
+                        <path
+                          d='M1.795 1.074L7.942 1.074 7.942 7.221M7.942 1.074L1.378 7.638'
+                          transform='translate(-935 -867) translate(836 848) translate(14 14) translate(85 5)'
+                        ></path>
+                      </g>
+                    </svg>
+                  </Link>
+                </li>
+                <li>
+                  <Link to={'https://jeju.kakao.com/'}>
+                    <span>제주 with kakao</span>
+                    <svg
+                      xmlns='http://www.w3.org/2000/svg'
+                      viewBox='0 0 9 9'
+                      className={styles.ico_outlink}
+                    >
+                      <g fill='none'>
+                        <path
+                          d='M1.795 1.074L7.942 1.074 7.942 7.221M7.942 1.074L1.378 7.638'
+                          transform='translate(-935 -867) translate(836 848) translate(14 14) translate(85 5)'
+                        ></path>
+                      </g>
+                    </svg>
+                  </Link>
+                </li>
+              </ul>
+            </div>
+
+            <div className={styles.txt_copyright}>
+              ©{' '}
+              <Link to={'https://www.kakaocorp.com/page'} target='_blank'>
+                Kakao Corp.
+              </Link>{' '}
+              All rights reserved.
+            </div>
+          </section>
         </div>
       </footer>
     </div>
