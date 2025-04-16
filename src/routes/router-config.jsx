4ae190ab9c1129e65802060f27ad5d34da622ebc
ehgus8 <68594605+ehgus8@users.noreply.github.com>
2025-04-16 14:09:09 +0900

    feat: 배포설정

--- src/routes/router-config.jsx
+++ src/routes/router-config.jsx
@@ -9,20 +9,25 @@ import MainTest from '../pages/main/MainPromise';
 import PressDetail from '../pages/presskit/PressDetail';
 
 // 라우터 설정
-export const router = createBrowserRouter([
+export const router = createBrowserRouter(
+  [
+    {
+      path: '/', // 부모 경로가 활성화 되었을 때 기본으로 사용할 컴포넌트
+      element: <RootLayout />,
+      errorElement: <div></div>,
+      children: [
+        {
+          index: true,
+          element: <Index />,
+        },
+        { path: 'searchKeyword', element: <SearchPage /> },
+        { path: 'presskit', element: <PressRelease /> },
+        { path: 'responsible/esg', element: <ESGPage /> }, // ESG 페이지 추가
+        { path: 'detail/:postId', element: <PressDetail /> },
+      ],
+    },
+  ],
   {
-    path: '/', // 부모 경로가 활성화 되었을 때 기본으로 사용할 컴포넌트
-    element: <RootLayout />,
-    errorElement: <div></div>,
-    children: [
-      {
-        index: true,
-        element: <Index />,
-      },
-      { path: 'searchKeyword', element: <SearchPage /> },
-      { path: 'presskit', element: <PressRelease /> },
-      { path: 'responsible/esg', element: <ESGPage /> }, // ESG 페이지 추가
-      { path: 'detail/:postId', element: <PressDetail /> },
-    ],
+    basename: '/kakaocorp-react-release',
   },
-]);
+);
